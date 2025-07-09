de602aa9b125f5a2388808bb1a526b37071322f9
Quang Bui <buivuphuquang0311@gmail.com>
2025-07-09 15:31:54 -0400

    mock data for account and customer

--- src/faker_generator.py
+++ src/faker_generator.py
@@ -13,20 +13,52 @@ class FakerGenerator:
         self.faker = Faker('vi_VN')
         self.schema = schema
         self.row_counts = row_counts
-        self.generated_cccd = set() # lookup time O(1)
+        self.generated_cccd = set() #lookup time O(1)
+        self.available_a_ids = random.sample(range(self.row_counts * 2), self.row_counts)
+        self.vpbank_account_header = ["10", "11", "12", "13", "14", "15", "16", "17", "18", "21", "69", "79", "82", "87"]
+        self.available_account_ids = self.generate_unique_account_ids()
+        self.available_u_ids = self.generate_unique_u_ids()
+        
+        
+    def generate_batch(self, u_id_list):
+        records = []
+
+        for u_id in u_id_list:
+            record = {}
+            for col, props in self.schema['columns'].items():
+                sdtype = props['sdtype']
+                if col == 'u_id':
+                    value = u_id
+                elif col == 'a_id':
+                    value = self.generate_a_id()
+                elif col == 'cccd':
+                    value = self.generate_cccd()
+                elif col == 'account_id':
+                    value = self.generate_account_id()
+                else:
+                    value = self.generate_values(sdtype, col)
+                record[col] = value
+            records.append(record)
+
+        return records
+
         
     def generate(self):
         records = []
         
-        for uid in range(0, self.row_counts):
+        for _ in range(0, self.row_counts):
             record = {}
             
             for col, props in self.schema['columns'].items():
                 sdtype = props['sdtype']
-                if col == 'UID':
-                    value = uid
+                if col == 'u_id':
+                    value = self.generate_u_id()
+                elif col == 'a_id':
+                    value = self.generate_a_id()
                 elif col == 'cccd':
                     value = self.generate_cccd()
+                elif col == 'account_id':
+                    value = self.generate_account_id()
                 else:
                     value = self.generate_values(sdtype, col)
                 
@@ -42,6 +74,35 @@ class FakerGenerator:
             if cccd not in self.generated_cccd:
                 self.generated_cccd.add(cccd)
                 return cccd
+    
+    def generate_unique_u_ids(self):
+        return list(range(self.row_counts))
+    
+    def generate_u_id(self):
+        return self.available_u_ids.pop(0) # customer table
+        # account table
+        ids = list(range(self.row_counts))
+        random.shuffle(ids)
+        return ids
+
+    def generate_a_id(self):
+        return self.available_a_ids.pop()
+    
+    def generate_unique_account_ids(self):
+        generated = set()
+        account_ids = []
+        while len(account_ids) < self.row_counts:
+            prefix = random.choice(self.vpbank_account_header)
+            suffix = str(random.randint(100000, 999999))  # 6 digits
+            acct_id = prefix + suffix  # 8-digit total
+            if acct_id not in generated:
+                generated.add(acct_id)
+                account_ids.append(acct_id)
+        random.shuffle(account_ids)
+        return account_ids
+            
+    def generate_account_id(self):
+        return self.available_account_ids.pop()
     
     def generate_values(self, sdtype, col):
         if col == 'first_name':
@@ -60,12 +121,14 @@ class FakerGenerator:
             return "Việt Nam"
         elif col == 'number':
             return self.faker.numerify("09# ### ####")
-        elif col == 'dob':
+        elif col == 'dob' or col == 'created_date':
             return self.faker.date()
         elif col == 'gender':
             return random.choice(["M", "F"])
         elif col == 'email':
             return self.faker.email()
+        elif col == 'account_status':
+            return "active"
         else:
             return None
         
--- src/main.py
+++ src/main.py
@@ -1,24 +1,49 @@
 from faker_generator import FakerGenerator
 from schema_manager import SchemaManager
 import pandas as pd
+import random
 import json
 
 def main():
+    '''
     # Load schema
     with open("data/customer_schema.json", "r") as f:
         schema = json.load(f)
 
-    # Validate schema
-    SchemaManager(schema)
+        # Validate schema
+        SchemaManager(schema)
 
-    # Generate fake data
-    faker = FakerGenerator(schema, row_counts=100)
-    data = faker.generate()
+        # Generate fake data
+        faker = FakerGenerator(schema, row_counts=10000)
+        data = faker.generate()
 
-    # Export to JSON
-    df = pd.DataFrame(data)
-    df.to_json("data/customer.json", orient="records", indent=2, force_ascii=False)
-    print("Data generation complete. Saved to 'customer.json'.")
+        # Export to JSON
+        df = pd.DataFrame(data)
+        df.to_json("data/customer.json", orient="records", lines=True, force_ascii=False)
+        
+        print("Data generation complete. Saved to 'customer.json'.")
+    '''
+    
+    #Batch creation for account
+    with open("data/account_schema.json", "r") as f:
+        schema = json.load(f)
+
+        SchemaManager(schema)
+
+        #Generate up to 2 accounts per user
+        u_ids = []
+        for uid in range(5000):
+            count = random.choice([1, 2])
+            u_ids.extend([uid] * count)
+        random.shuffle(u_ids)
+
+        #Generate data using batch
+        faker = FakerGenerator(schema, row_counts=len(u_ids))
+        data = faker.generate_batch(u_ids)
+
+        #Save to JSON
+        df = pd.DataFrame(data)
+        df.to_json("data/account.json", orient="records", lines=True, force_ascii=False)
 
 if __name__ == "__main__":
     main()
